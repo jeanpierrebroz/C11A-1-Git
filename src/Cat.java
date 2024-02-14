e0b86d28ee455386dafdb59c9834ab4580d94a8a
drewruana <drew@ruanafamily.com>
2024-02-14 15:17:33 -0700

    On branch main
    	-changed the meow to MEOOOOWWWW
    Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/Cat.java
    
     Changes not staged for commit:
    	modified:   .gitignore

--- src/Cat.java
+++ src/Cat.java
@@ -47,6 +47,8 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		
+		//changed meow to a lot more meow
+		g2.drawString("MEEEEEEOWWWWWWW", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
